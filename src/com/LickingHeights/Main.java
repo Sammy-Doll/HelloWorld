f34928528d9c9fa06669969da6d287ac0e507e6b
Sammy Doll <samuel.doll.2023@student.lhschools.org>
2020-02-24 13:53:23 -0500

    Intellij Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,16 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+        System.out.print("My name is ");
+        System.out.println("Thjnp");
+        System.out.println("and I have a B-22 intergalactic missile.");
+        System.out.print("If you 'humans' do not surrender Earth ");
+        System.out.println("to the Zargoth race, ");
+        System.out.print("then ")
+        System.out.println("your entire planet will be terminated.");
+        System.out.println("You have 48 hours.");
+    }
+}
